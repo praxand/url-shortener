23b3ef49bb4d3052a7f00504b8973d3320f5fe9b
praxand <jay.cheung2005@gmail.com>
2023-11-07 13:45:06 +0100

    components

--- resources/views/shortlinks/partials/empty.blade.php
+++ resources/views/shortlinks/partials/empty.blade.php
@@ -36,12 +36,9 @@
             >
                 <span class="inline-flex">
                     <a href="{{ route('shortlinks.create') }}">
-                        <button
-                            type="button"
-                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
+                        <x-primary-button>
                             New shortlink
-                        </button>
+                        </x-primary-button>
                     </a>
                 </span>
             </div>

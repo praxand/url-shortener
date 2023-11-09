f6b671d01c536943855213185871fa6abe58419e
praxand <jay.cheung2005@gmail.com>
2023-11-09 10:25:59 +0100

    fixed animation

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,18 +1,21 @@
 <div
     x-data="{ show: true }"
-    x-init="setTimeout(() => show = false, 5000)"
+    x-init="setTimeout(() => show = false, 3000)"
     class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
 >
-    <div x-show="show"
-    class="flex w-full flex-col items-center space-y-4 sm:items-end"
-    x-transition
-            x-transition:enter="transform ease-out duration-300 transition"
-            x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-            x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
-            x-transition:leave="transition ease-in duration-100"
-            x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0">
-        <div class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+    <div
+        x-show="show"
+        class="flex w-full flex-col items-center space-y-4 sm:items-end"
+        x-transition:enter="transform ease-out duration-300 transition"
+        x-transition:enter-start="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        x-transition:enter-end="translate-y-0 opacity-100 sm:translate-x-0"
+        x-transition:leave="transition ease-in duration-100"
+        x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0"
+    >
+        <div
+            class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5"
+        >
             <div class="p-4">
                 <div class="flex items-start">
                     <div class="flex-shrink-0">

d4b3f4fa7bb86dc36117a4e711a2c9b92a801fcc
praxand <jay.cheung2005@gmail.com>
2023-11-12 00:18:28 +0100

    removed unused import

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuickResponseCodeController;
 use App\Http\Controllers\RedirectController;
-use App\Http\Controllers\ShortlinkController;
-use App\Models\Shortlink;
+use App\Http\Controllers\ShortlinkController;g
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
